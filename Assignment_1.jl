ff5f93fd1c3f0e9a15c92992be1debb85be75677
Eric Rockstädt <eric_rockstaedt@yahoo.de>
2020-05-28 15:43:43 +0200

    deleted test file, uploaded solution johannes

---
+++ Assignment_1.jl
@@ -0,0 +1,34 @@
+using DataFrames
+using CSV
+
+# creating a DataFrames
+
+data = DataFrame()
+
+# creating arrays for each column
+technology = ["Nuclear","Lignite","HardCoal","NaturalGas"]
+FuelCost   = [3,6.21,10.6,31.08]
+𝜂        = [0.33,0.42,0.42,0.59]
+varCost    = [10,6,6,2]
+λ          = [0,.399,.337,.201]
+
+#As no CO2 price was given, we used the price from eex dated 26.05.20
+CO2Cost    = 21.71
+
+#Filling the DataFrame
+data = DataFrame(technology=technology,FuelCost=FuelCost,efficiency=𝜂,OM=varCost,emissionFactor=λ)
+
+
+#Not necessary steps, just to get used to csv :)
+CSV.write("data.csv",data)
+powerplants= CSV.read("data.csv")
+
+#Calculating marginal cost by applying the formula on the columns elementwise
+newcol = (FuelCost./𝜂)+CO2Cost*λ.+varCost
+
+#attaching the results as column to already existing DataFrame
+insertcols!(powerplants,:MC =>newcol)
+
+powerplants
+
+CSV.write("results.csv",powerplants)
